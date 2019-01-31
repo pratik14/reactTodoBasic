ba4bbae924bcc9a0370df24d0453e9de379dca59
Pratik Shah <pratik14shah@gmail.com>
2019-01-31 19:17:22 +0530

    checkbox are readOnly

--- src/todoItem.js
+++ src/todoItem.js
@@ -25,7 +25,7 @@ class TodoItem extends Component {
     
     return(
       <li>
-        <input type="checkbox"  checked={todo.completed} onClick={this.markCompleted} />
+        <input type="checkbox"  checked={todo.completed} onClick={this.markCompleted} readOnly />
         <label>{todo.name}</label>
         <button onClick={this.removeItem}>Remove</button>
       </li> 
